bb0cf5071c24962ada91ca0e97b17f0cc1e55dc3
Yağız <yagiz.tansu99@gmailo.com>
2021-10-10 19:16:05 +0300

    deleted useless js mig ans css file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @include('header')
 <div class="owl-carousel owl-theme">
-    <div class="owl-slide d-flex align-items-center cover" style="background-image: url('assets/img/about/plane1.jpg');">
+    <div class="owl-slide d-flex align-items-center cover" style="background-image: url('assets/img/about/PLANE.png');">
         <div class="container">
             <div class="row justify-content-center justify-content-md-start">
                 <div class="col-10 col-md-6 static">
@@ -21,23 +21,11 @@
             </div>
         </div>
     </div><!-- /slide1 -->
-    <div class="owl-slide d-flex align-items-center cover" style="background-image: url('assets/img/about/PLANE.png');">
-        <div class="container">
-            <div class="row justify-content-center justify-content-md-start">
-                <div class="col-10 col-md-6 static">
-                    <div class="owl-slide-animated owl-slide-text">
-
-                    </div><!-- /owl-slide-text -->
-                </div>
-            </div>
-        </div>
-    </div><!-- /slide1 -->
     <div class="owl-slide d-flex align-items-center cover" style="background-image: url('assets/img/about/cur1.jpg');">
         <div class="container">
             <div class="row justify-content-center justify-content-md-start">
                 <div class="col-10 col-md-6 static">
                     <div class="owl-slide-animated owl-slide-text">
-
                     </div><!-- /owl-slide-text -->
                 </div>
             </div>
@@ -53,69 +41,69 @@
                             <h2>HİZMETLER</h2>
                         </div>
             <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-cat text-center mb-50" >
+                <div class="col-lg-4 col-md-6 col-sm-6 mb-50">
+                    <div class="single-cat text-center" >
                         <div class="cat-icon">
                             <span class="flaticon-shipped"></span>
                         </div>
                         <div class="cat-cap">
                             <h5><a href="/karayolları" >Kara Taşımacılığı</a></h5>
-                            <p>Mobil iletişim sistemleri ile donatılmış geniş araç filosu, gelişmiş bilgi teknolojileriyle güçlendirilmiş altyapısı ve uzman personeliyle Global Gate Lojistik, kaliteli hizmet ilkesi doğrultusunda müşterilerine komple araç ve parsiyel taşıma hizmeti vermektedir.</p>
+                            <p>Tamamı çevre koruma standartlarına uygun yeşil ve sessiz motor özellikleri ve Uydu Takip sistemi ile donatılmış olan özmal araç filosuyla; Avrupa Kıtasına yönelik komple/parsiyel taşımaları uluslararası karayolu nakliye rejimine uygun olarak gerçekleştirmektedir.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-cat text-center mb-50" >
+                <div class="col-lg-4 col-md-6 col-sm-6 mb-50">
+                    <div class="single-cat text-center" >
                         <div class="cat-icon">
                             <span class="flaticon-ship"></span>
                         </div>
                         <div class="cat-cap">
                             <h5><a href="/denizyolu">Deniz Taşımacılığı</a></h5>
-                            <p>Mobil iletişim sistemleri ile donatılmış geniş araç filosu, gelişmiş bilgi teknolojileriyle güçlendirilmiş altyapısı ve uzman personeliyle Global Gate Lojistik, kaliteli hizmet ilkesi doğrultusunda müşterilerine komple araç ve parsiyel taşıma hizmeti vermektedir.</p>
+                            <p>Global Garte Lojistik ile sahip olduğumuz güçlü ağ sayesinde; dünya genelinde dilerseniz limandan limana, dilerseniz kapıdan kapıya hizmet sunmaktayız.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-cat text-center mb-50 " >
+                <div class="col-lg-4 col-md-6 col-sm-6  mb-50 ">
+                    <div class="single-cat text-center" >
                         <div class="cat-icon">
                             <span class="flaticon-plane"></span>
                         </div>
                         <div class="cat-cap">
                             <h5><a href="/havayolu">Hava Taşımacılığı</a></h5>
-                            <p>Mobil iletişim sistemleri ile donatılmış geniş araç filosu, gelişmiş bilgi teknolojileriyle güçlendirilmiş altyapısı ve uzman personeliyle Global Gate Lojistik, kaliteli hizmet ilkesi doğrultusunda müşterilerine komple araç ve parsiyel taşıma hizmeti vermektedir.</p>
+                            <p>Bağımsız partnerleri ile yükünüzü taşımacılığın ötesinde profesyonel bir sistem sunarak, dünyanın her noktasına, en hızlı ve güvenli biçimde ulaştırmaktayız.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-cat text-center mb-50" >
+                <div class="col-lg-4 col-md-6 col-sm-6  mb-50">
+                    <div class="single-cat text-center" >
                         <div class="cat-icon">
                             <span class="flaticon-shipped"></span>
                         </div>
                         <div class="cat-cap">
                             <h5><a href="/minivan" >Minivan Taşımacılığı</a></h5>
-                            <p>Mobil iletişim sistemleri ile donatılmış geniş araç filosu, gelişmiş bilgi teknolojileriyle güçlendirilmiş altyapısı ve uzman personeliyle Global Gate Lojistik, kaliteli hizmet ilkesi doğrultusunda müşterilerine komple araç ve parsiyel taşıma hizmeti vermektedir.</p>
+                            <p>Terminli olan ihracat ve ithalat yüklemeleriniz için, havayoluna alternatif olan özel minivan araçlarımızla, Avrupa geneline/ genelinden 36-48 saatte garantili teslimat imkanı sunmaktadır.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-cat text-center mb-50" >
+                <div class="col-lg-4 col-md-6 col-sm-6  mb-50">
+                    <div class="single-cat text-center" >
                         <div class="cat-icon">
                             <span class="flaticon-ship"></span>
                         </div>
                         <div class="cat-cap">
                             <h5><a href="/proje">Proje Taşımacılığı</a></h5>
-                            <p>Mobil iletişim sistemleri ile donatılmış geniş araç filosu, gelişmiş bilgi teknolojileriyle güçlendirilmiş altyapısı ve uzman personeliyle Global Gate Lojistik, kaliteli hizmet ilkesi doğrultusunda müşterilerine komple araç ve parsiyel taşıma hizmeti vermektedir.</p>
+                            <p>Global Gate olarak ağır taşımacılıkta inşaat, enerji ve maden sektörü olmak üzere pek çok sektörde kullanılan makine ve ekipmanı her destinasyona güvenle ulaştırma olanağına sahibiz.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-cat text-center mb-50 " >
+                <div class="col-lg-4 col-md-6 col-sm-6  mb-50">
+                    <div class="single-cat text-center" >
                         <div class="cat-icon">
                             <span class="flaticon-plane"></span>
                         </div>
                         <div class="cat-cap">
                             <h5><a href="/depolama">Depolama</a></h5>
-                            <p>Mobil iletişim sistemleri ile donatılmış geniş araç filosu, gelişmiş bilgi teknolojileriyle güçlendirilmiş altyapısı ve uzman personeliyle Global Gate Lojistik, kaliteli hizmet ilkesi doğrultusunda müşterilerine komple araç ve parsiyel taşıma hizmeti vermektedir.</p>
+                            <p>Global Gate Lojistik Depo yönetim sistemi, sipariş yönetimi, envanter kontrol, depo operasyonları ve sevkıyatlar gerçekleştirilmektedir.</p>
                         </div>
                     </div>
                 </div>
@@ -124,7 +112,7 @@
     </div>
     <!-- Categories Area End -->
     <!--? About Area Start -->
-    <div class="about-low-area pt-5 pb-5">
+    <div class="about-low-area pt-5">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12">
@@ -133,20 +121,16 @@
                         <div class="section-tittle text-center mb-35">
                             <h2>GLOBAL GATE LOJİSTİK HAKKINDA</h2>
                         </div>
-                        <p>Tüm lojistik ihtiyaçlarınızda doğru çözüm; ASL LOJİSTİK
-
-                        ASL Lojistik, tüm lojistik ihtiyaçlarınız için en doğru seçimdir. ASL’ nin uzman ekibi tarafından deneyim ve teknoloji; hız, zaman ve maliyet avantajı ile birleştirilerek, kaliteli hizmet ilkesi izlenir. ASL Lojistik’ in firmanıza katacağı değerler; ASL ile tüm lojistik yükünüzü uzman kadromuza devrederek, Lojistik için ayırdığınız insan gücünü, finansmanı ve zamanı ana faaliyet alanınıza yönlendirebilirsiniz.
-
-                        ASL ile ürünlerinizi, pazarlama ve tedarik alanlarının her noktasına/noktasından en uygun maliyetle en hızlı şekilde ulaştırırsınız. ASL ile planlı ve zamanında teslimatla, stok seviyelerinizi minimuma indirerek stok maliyetlerinizde büyük avantaj sağlarsınız.
-
-                        ASL ile hasar ve kaybolma gibi riskleri ASL’ ye devrederek, beklenmeyen maliyetlerinizden kurtulursunuz.</p>
+                        <p>Global Gate Lojistik, tüm lojistik ihtiyaçlarınız için en doğru seçimdir. Global Gate’ nin uzman ekibi tarafından deneyim ve teknoloji; hız, zaman ve maliyet avantajı ile birleştirilerek, kaliteli hizmet ilkesi izlenir. Global Gate Lojistik’ in firmanıza katacağı değerler; Global Gate ile tüm lojistik yükünüzü uzman kadromuza devrederek, Lojistik için ayırdığınız insan gücünü, finansmanı ve zamanı ana faaliyet alanınıza yönlendirebilirsiniz.</p>
+                        <p>Global Gate ile ürünlerinizi, pazarlama ve tedarik alanlarının her noktasına/noktasından en uygun maliyetle en hızlı şekilde ulaştırırsınız. Global Gate ile planlı ve zamanında teslimatla, stok seviyelerinizi minimuma indirerek stok maliyetlerinizde büyük avantaj sağlarsınız.</p>
+                        <p>Global Gate ile hasar ve kaybolma gibi riskleri Global Gate’ ye devrederek, beklenmeyen maliyetlerinizden kurtulursunuz.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-        <div class="padding-bottom">
+        <div class="section-padding">
         <div class="container">
             <div class="row">
                 <div class="col-lg-5 col-md-12">
