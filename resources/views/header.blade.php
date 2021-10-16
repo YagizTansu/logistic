719e977b07936125fd86f75174fd8fd55065ba4c
Yağız <yagiz.tansu99@gmailo.com>
2021-10-16 13:25:27 +0300

    meta data deleted

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,10 +1,9 @@
 <!doctype html>
-<html class="no-js" lang="zxx">
+<html>
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Global Gate Lojistik. Uluslararası kara, hava ve deniz yolu taşımacılığı.</title>
-    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSS here -->
@@ -13,117 +12,118 @@
     <link rel="stylesheet" href="{{asset('assets/css/fontawesome-all.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/themify-icons.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-   
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
-    
+
 
 
 </head>
+
 <body>
-<!--? Preloader Start -->
-<div id="preloader-active">
-    <div class="preloader d-flex align-items-center justify-content-center">
-        <div class="preloader-inner position-relative">
-            <div class="preloader-circle"></div>
-            <div class="preloader-img pere-text">
-                <img src="{{asset('assets/img/logo/logo2.png')}}" alt="">
+    <!--? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="{{asset('assets/img/logo/logo2.png')}}" alt="">
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- Preloader Start -->
-<header>
-    <!-- Header Start -->
-    <div class="header-area">
-        <div class="main-header">
-            <div class="header-top d-none d-lg-block">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-9 col-md-6 col-sm-6">
-                            <div class="row d-flex justify-content-between align-items-center">
-                                <div class="header-info-left">
-                                    <ul>
-                                        <li><span class="contact-info__icon "><i class="ti-tablet"></i></span>+99 (0) 101 0000 888</li>
-                                        <li><span class="contact-info__icon"><i class="ti-email"></i></span> globalgate@gmail.com</li>
+    <!-- Preloader Start -->
+    <header>
+        <!-- Header Start -->
+        <div class="header-area">
+            <div class="main-header">
+                <div class="header-top d-none d-lg-block">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-9 col-md-6 col-sm-6">
+                                <div class="row d-flex justify-content-between align-items-center">
+                                    <div class="header-info-left">
+                                        <ul>
+                                            <li><span class="contact-info__icon "><i class="ti-tablet"></i></span>+99 (0) 101 0000 888</li>
+                                            <li><span class="contact-info__icon"><i class="ti-email"></i></span> globalgate@gmail.com</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-sm-6">
+                                <div class="header-info-right">
+                                    <ul class="header-social">
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                        <li> <a href="#"><i class="fab fa-google-plus-g"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-6 col-sm-6">
-                            <div class="header-info-right">
-                                <ul class="header-social">
-                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                                    <li> <a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="header-bottom  ">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <!-- Logo -->
-                        <div class="col-xl-2 col-lg-2">
-                            <div class="logo logo-padding">
-                                <a href="/anasayfa"><img  src="{{asset('assets/img/logo/logo2.png')}}" alt=""></a>
+                <div class="header-bottom  ">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <!-- Logo -->
+                            <div class="col-xl-2 col-lg-2">
+                                <div class="logo logo-padding">
+                                    <a href="/anasayfa"><img src="{{asset('assets/img/logo/logo2.png')}}" alt=""></a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-xl-10 col-lg-10 col-md-12">
-                            <div class="menu-wrapper  d-flex align-items-center justify-content-end">
-                                <!-- Main-menu -->
-                                <div class="main-menu d-none d-lg-block">
-                                    <nav>
-                                        <ul id="navigation">
-                                            <li><a href="/anasayfa">ANA SAYFA</a></li>
-                                            <li><a href="/kurumsal">KURUMSAL</a>
-                                                <ul class="submenu">
-                                                    <li><a href="/kurumsal">Kurumsal</a></li>
-                                                    <li><a href="/arac-filomuz">Araç Filomuz</a></li>
-                                                    <li><a href="/haberler">Haberler</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="/karayolu">HİZMETLER</a>
-                                                <ul class="submenu">
-                                                    <li><a href="/karayolu">Karayolu Taşımacılığı</a></li>
-                                                    <li><a href="/havayolu">Havayolu Taşımacılığı</a></li>
-                                                    <li><a href="/denizyolu">Denizyolu Taşımacığı</a></li>
-                                                    <li><a href="/minivan">Minivan Taşımacılığı</a></li>
-                                                    <li><a href="/proje">Proje Taşımacılığı</a></li>
-                                                    <li><a href="/depolama">Depolama</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="/incoterms">SEKTÖREL BİLGİLER</a>
-                                                <ul class="submenu">
-                                                    <li><a href="incoterms">Incoterms</a></li>
-                                                    <li><a href="/dis-ticaret-kisaltmalari">Dış Ticaret Kısaltmaları</a></li>
-                                                    <li><a href="/uld-ölcüleri">ULD ölçüleri</a></li>
-                                                    <li><a href="/konteyner-ölcüleri">Konteyner ölçüleri</a></li>
-                                                    <li><a href="/kargo-hacim-hesaplama">Kargo hacim hesaplama</a></li>
-                                                    <li><a href="/tir-ölcüleri">Tır ölçüleri</a></li>
+                            <div class="col-xl-10 col-lg-10 col-md-12">
+                                <div class="menu-wrapper  d-flex align-items-center justify-content-end">
+                                    <!-- Main-menu -->
+                                    <div class="main-menu d-none d-lg-block">
+                                        <nav>
+                                            <ul id="navigation">
+                                                <li><a href="/anasayfa">ANA SAYFA</a></li>
+                                                <li><a href="/kurumsal">KURUMSAL</a>
+                                                    <ul class="submenu">
+                                                        <li><a href="/kurumsal">Kurumsal</a></li>
+                                                        <li><a href="/arac-filomuz">Araç Filomuz</a></li>
+                                                        <li><a href="/haberler">Haberler</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="/karayolu">HİZMETLER</a>
+                                                    <ul class="submenu">
+                                                        <li><a href="/karayolu">Karayolu Taşımacılığı</a></li>
+                                                        <li><a href="/havayolu">Havayolu Taşımacılığı</a></li>
+                                                        <li><a href="/denizyolu">Denizyolu Taşımacığı</a></li>
+                                                        <li><a href="/minivan">Minivan Taşımacılığı</a></li>
+                                                        <li><a href="/proje">Proje Taşımacılığı</a></li>
+                                                        <li><a href="/depolama">Depolama</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="/incoterms">SEKTÖREL BİLGİLER</a>
+                                                    <ul class="submenu">
+                                                        <li><a href="incoterms">Incoterms</a></li>
+                                                        <li><a href="/dis-ticaret-kisaltmalari">Dış Ticaret Kısaltmaları</a></li>
+                                                        <li><a href="/uld-ölcüleri">ULD ölçüleri</a></li>
+                                                        <li><a href="/konteyner-ölcüleri">Konteyner ölçüleri</a></li>
+                                                        <li><a href="/kargo-hacim-hesaplama">Kargo hacim hesaplama</a></li>
+                                                        <li><a href="/tir-ölcüleri">Tır ölçüleri</a></li>
 
-                                                </ul>
-                                            </li>                                            
-                                        </ul>
-                                    </nav>
-                                </div>
-                                
-                                <!-- Header-btn -->
-                                <div class="header-right-btn d-none d-lg-block ml-20">
-                                    <a href="/iletisim" class="btn-style">İLETİŞİM/TEKLİF AL</a>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+
+                                    <!-- Header-btn -->
+                                    <div class="header-right-btn d-none d-lg-block ml-20">
+                                        <a href="/iletisim" class="btn-style">İLETİŞİM/TEKLİF AL</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- Mobile Menu -->
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
+                            <!-- Mobile Menu -->
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
